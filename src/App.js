59a633238bd7379cf953939909a46dabd8a08834
scarter31191 <scarter31191@gmaile.com>
2021-08-09 21:52:48 -0400

    added home and about route. also created about file and changed links in pagewrapper components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import PageWrapper from './components/PageWrapper';
 import Home from './components/Pages/Home';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import About from './components/Pages/About';
 
 function App() {
   return (
-    <PageWrapper>
-      <Home />
-    </PageWrapper>
+    <Router>
+      <PageWrapper>
+
+        <Route 
+          exact={true}
+          path="/"
+          component={Home}
+        />
+
+        <Route 
+          path="/about"
+          component={About}
+        />
+
+
+      </PageWrapper>
+    </Router>
   );
 }
 
